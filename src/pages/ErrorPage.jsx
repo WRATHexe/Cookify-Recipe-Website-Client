50161f681f7857e4d051ebcae1cb65bb0a41e4a9
WRATHeXe <FardinRuhian001@gmail.com>
2025-06-12 02:41:44 +0600

    Add Lottie animation to ErrorPage and update styling
    - Integrated Lottie animation for a more engaging 404 error experience.
    - Changed background color to orange for better visibility.
    - Adjusted layout to include animation above the error message.

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,9 +1,15 @@
+import Lottie from "lottie-react";
 import { Link } from "react-router";
+import foodAnimation from "../assets/foodAnimation.json";
 
 const ErrorPage = () => {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-slate-50 px-4">
-      <h1 className="text-9xl font-extrabold text-slate-800 mb-6">404</h1>
+    <div className="min-h-screen flex flex-col items-center justify-center bg-orange-50 px-4">
+      {/* Lottie Animation */}
+      <div className="w-64 h-64 mb-2 ">
+        <Lottie animationData={foodAnimation} loop={true} />
+      </div>
+
       <p className="text-2xl sm:text-3xl font-semibold text-slate-700 mb-4">
         Oops! Page not found.
       </p>
